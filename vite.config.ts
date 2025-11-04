35267589be189469709449f5ef66245dceef1ff1
coolkim16 <hans1226b@gmail.com>
2025-11-04 15:58:05 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -217,7 +217,7 @@ export default defineConfig(({ mode }) => {
       componentTagger(),
       cdnPrefixImages(),
     ].filter(Boolean),
-    base: '/pump-selection-app/'
+    base: '/pump-selection-app/',
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
